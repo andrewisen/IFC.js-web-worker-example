f5d9ff76c9b90fa282eb88f1fc94b1bb3e8aa9e4
André "MG" Wisén <andre.wisen@gmail.com>
2021-01-19 13:10:47 +0100

    Add config for Web Worker

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,63 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-  input: './src/IFC.js',
-  external: ['three', 'chevrotain'],
-  output: [
-    {
-      file: './build/IFC.module.js',
-      format: 'es',
-      globals: {
-        three: 'THREE',
-        chevrotain: 'chevrotain'
+export default [
+  {
+    input: './src/IFC.geometry.js',
+    external: ['three'],
+    output: [
+      {
+        file: './build/IFC.geometry.module.js',
+        format: 'es',
+        globals: {
+          three: 'THREE'
+        }
+      },
+      {
+        file: './build/IFC.geometry.js',
+        format: 'iife',
+        name: 'IFCjs',
+        globals: {
+          three: 'THREE'
+        }
       }
-    },
-    {
-      file: './build/IFC.js',
-      format: 'iife',
-      name: 'IFCjs',
-      globals: {
-        three: 'THREE',
-        chevrotain: 'chevrotain'
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**', 'libs/**', 'examples/**'],
+        babelHelpers: 'bundled'
+      }),
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input: './src/IFC.worker.js',
+    external: ['chevrotain'],
+    output: [
+      {
+        file: './build/IFC.worker.module.js',
+        format: 'es',
+        globals: {
+          chevrotain: 'chevrotain'
+        }
+      },
+      {
+        file: './build/IFC.worker.js',
+        format: 'iife',
+        name: 'IFCjs',
+        globals: {
+          chevrotain: 'chevrotain'
+        }
       }
-    }
-  ],
-  plugins: [
-    babel({
-      exclude: ['node_modules/**', 'libs/**'],
-      babelHelpers: 'bundled'
-    }),
-    resolve(),
-    commonjs()
-  ]
-};
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**', 'libs/**', 'examples/**'],
+        babelHelpers: 'bundled'
+      }),
+      resolve(),
+      commonjs()
+    ]
+  }
+];
