f8cd7692758f574a6c85a75b28cc8e5ecdc2a34f
André "MG" Wisén <andre.wisen@gmail.com>
2021-01-19 13:11:07 +0100

    Add Geometry bundle

---
+++ src/IFC.geometry.js
@@ -0,0 +1,5 @@
+/**
+ * For building geometry OUTSIDE (!) of the Web Worker; requires Three
+ */
+export { buildGeometry } from './ifc-to-three.js/converter.js';
+export { mainObject } from './ifc-to-three.js/scene/mainObject.js';
