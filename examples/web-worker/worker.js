ca2342c48a3b8d0e1ca93b00384577555233a9a1
André "MG" Wisén <andre.wisen@gmail.com>
2021-01-21 11:46:01 +0100

    Update comments

--- examples/web-worker/worker.js
+++ examples/web-worker/worker.js
@@ -1,19 +1,23 @@
 /**
- * See:
- * https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers#importing_scripts_and_libraries
+ * Please note that FireFox doesn't support module workers.
+ * We will use the regular importScripts().
+ * That's why we import the regular builds (not "IFC.module.js").
+ *
+ * Also, make sure to import IFC.js last.
+ *
+ * See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers#importing_scripts_and_libraries
  */
+
 importScripts('../libs/chevrotain.min.js');
-// importScripts('../libs/three.js');
-/**
- * NB. We import the regular build.
- * Also, make sure to import "IFC.js" last (!)
- */
 importScripts('../../build/IFC.worker.js');
+
+/**
+ * See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers#sending_messages_to_and_from_a_dedicated_worker
+ */
 onmessage = (e) => {
-  const ifcData = e.data; // See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers#sending_messages_to_and_from_a_dedicated_worker
+  const ifcData = e.data;
   const loaded = IFCjs.loadIfcFileItems(ifcData);
   const structured = IFCjs.constructProject(loaded);
-
   /**
    * The Web Worker cannot post the MainObject.
    * It will give the following error:
@@ -25,14 +29,8 @@ onmessage = (e) => {
    *
    * This is because MainObject cannot be cloned using the structured clone algorithm.
    *
-   * See:
-   * https://developer.mozilla.org/en-US/docs/Web/API/Worker/postMessage
-   */
-
-  structured.MainObject = undefined; // bye bye
-
-  /**
-   * See: https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers#sending_messages_to_and_from_a_dedicated_worker
+   * See: https://developer.mozilla.org/en-US/docs/Web/API/Worker/postMessage
+   * And: https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers#sending_messages_to_and_from_a_dedicated_worker
    */
   postMessage(structured);
 };
