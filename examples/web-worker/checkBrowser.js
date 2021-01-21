57afd03d17a3fee655d5a305cd7bbb84b86720fd
André "MG" Wisén <andre.wisen@gmail.com>
2021-01-21 11:38:58 +0100

    Add comments

--- examples/web-worker/checkBrowser.js
+++ examples/web-worker/checkBrowser.js
@@ -1,4 +1,11 @@
-console.log(navigator.userAgent);
+/**
+ * Alert WebKit users.
+ *
+ * Safari can't spawn new Web Workers inside Web Workers.
+ * I.e. use nested Web Workers.
+ *
+ * See: https://www.chromestatus.com/feature/6080438103703552
+ */
 if (navigator.userAgent.indexOf('Safari') !== -1) {
   alert('Your browser is not supported at the moment. Please switch to Chrome or Firefox.');
 }
