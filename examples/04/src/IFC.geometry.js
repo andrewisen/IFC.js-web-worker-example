fd581ee1bacc62ab931bb372edd13a55b8b06fea
André "MG" Wisén <andre.wisen@gmail.com>
2021-02-16 16:34:05 +0100

    Add Earcut

--- examples/04/src/IFC.geometry.js
+++ examples/04/src/IFC.geometry.js
@@ -1,5 +1,5 @@
 /**
  * For building geometry OUTSIDE (!) of the Web Worker; requires Three
  */
-export { buildGeometry } from '../../src/ifc-to-three.js/converter.js';
-export { mainObject } from '../../src/ifc-to-three.js/scene/mainObject.js';
+export { buildGeometry } from '../../../src/ifc-to-three.js/converter.js';
+export { mainObject } from '../../../src/ifc-to-three.js/scene/mainObject.js';
--- examples/04/src/IFC.multiWorker.js
+++ examples/04/src/IFC.multiWorker.js
@@ -1,6 +1,6 @@
 /**
  * For Multi Web Worker; requires Chevrotain
  */
-export { bindEntities } from '../../src/ifc-parser/ifc-services/ifc-items-referencer.js';
-export { extractSections } from '../../src/ifc-parser/ifc-services/ifc-items-reader.js';
-export { constructProject } from '../../src/ifc-project-builder/ifc-structure-builder.js';
+export { bindEntities } from '../../../src/ifc-parser/ifc-services/ifc-items-referencer.js';
+export { extractSections } from '../../../src/ifc-parser/ifc-services/ifc-items-reader.js';
+export { constructProject } from '../../../src/ifc-project-builder/ifc-structure-builder.js';
